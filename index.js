77b9cb5e212e9ae457846abfa3629beb29403786
markcyfred <dcsnrb594822@spu.ac.ke>
2025-10-15 14:27:39 +0300

    javascript work

---
+++ index.js
@@ -0,0 +1,37 @@
+// document.getElementById('mysubmit').onclick= 
+// function()
+// {
+//      username = document.getElementById('mytext').value;
+
+// document.getElementById('show').textContent=(`${username}`)
+// }
+
+// let username;
+
+// document.getElementById('mysubmit').onclick = 
+// function () {
+//      username = document.getElementById('mytext').value;
+
+//      document.getElementById('myname').textContent =(`my name is ${username}`);
+// }
+
+// let username;
+
+// document.getElementById('mysubmit').onclick=
+// function () {
+//      username = document.getElementById('mytext').value;
+//      document.getElementById('myname').textContent =(`my name is ${username}`);
+
+// }
+
+
+
+
+
+
+
+let username = 'Mark Kinai'
+
+// console.log(`uesrname`)
+
+document.getElementById('username').textContent=(`this is testing mpesa ${username}`);
